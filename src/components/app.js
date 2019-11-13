fe96a3f1381f4b88cc6f4471d9c42fd4826ca92e
David Golightly <davigoli@gmail.com>
2019-11-13 11:12:36 -0800

    customize eslint rules

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,7 @@
-import { h, Component } from 'preact';
+import { h } from 'preact'
 
-export default class App extends Component {
-
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
-	render() {
-		return (
-			<div id="app">
-				<h1>What is Bezos worth?</h1>
-			</div>
-		);
-	}
-}
+export const App = () => (
+	<div id="app">
+		<h1>What is Bezos worth?</h1>
+	</div>
+)
