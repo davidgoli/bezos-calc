34ffc5697e7265066413ea14be381183fbcf93cc
David Golightly <davigoli@gmail.com>
2019-11-13 12:58:26 -0800

    display affordable units lost

--- src/components/app.js
+++ src/components/app.js
@@ -15,11 +15,15 @@ const HEAD_TAX_REVENUE_PER_SECOND = ANNUAL_HEAD_TAX_REVENUE / 365 / 24 / 60 / 60
 const HEAD_TAX_START_DATE = new Date('May 18, 2018')
 const AMAZON_HEAD_TAX_PCT = 0.3
 
+// https://www.king5.com/article/news/local/building-affordable-housing-in-seattle-isnt-cheap/281-552498112
+const AFFORDABLE_HOUSING_UNIT_COST = 300000
+
 export const App = () => {
 	const { response, loading, error } = useFetch('https://forbes400.herokuapp.com/api/forbes400?limit=10')
 
 	const [netWorth, setNetWorth] = useState(0)
 	const [headTaxRevenue, setHeadTaxRevenue] = useState(0)
+
 	useEffect(() => {
 		setTimeout(() => {
 			if (netWorth === 0) {
@@ -45,10 +49,15 @@ export const App = () => {
 	if (netWorth === 0) {
 		setNetWorth(bezos.finalWorth * 1000)
 	}
+	console.log(bezos)
+
+	const totalHTRevenue = headTaxRevenue / AMAZON_HEAD_TAX_PCT
+	const affordableUnits = Math.floor(totalHTRevenue / AFFORDABLE_HOUSING_UNIT_COST)
 
 	return (<div id="app">
 		<h1>What is Jeff Bezos worth? {formatCurrency(netWorth)}</h1>
 		<h2>How much Head Tax revenue Amazon would have paid: {formatCurrency(headTaxRevenue)}</h2>
 		<h2>Total Head Tax revenue from all companies in Seattle: {formatCurrency(headTaxRevenue / AMAZON_HEAD_TAX_PCT)}</h2>
+		<h2>Affordable housing units lost: {affordableUnits}</h2>
 	</div>)
 }
