d9972154c9a4fb6421ffa276710da077251bc3a6
David Golightly <davigoli@gmail.com>
2019-11-13 12:14:49 -0800

    fetch & display bezos' net worth

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,24 @@
 import { h } from 'preact'
+import { useFetch } from './useFetch'
 
-export const App = () => (
-	<div id="app">
-		<h1>What is Bezos worth?</h1>
-	</div>
-)
+const findJeff = (billionaires) => billionaires.find((b) => b.person.name === 'Jeff Bezos')
+const formatCurrency = (thousands) => `$${(thousands * 1000).toFixed(0).split(/([0-9]{3})/g).filter(s => s.length > 0).join(',')}`
+
+export const App = () => {
+	const { response, loading, error } = useFetch('https://forbes400.herokuapp.com/api/forbes400?limit=10')
+	if (loading) {
+		return <div>Loading...</div>
+	}
+
+	if (error) {
+		console.error(error)
+		return <div>Error: {error}</div>
+	}
+
+	const bezos = findJeff(response)
+	const netWorth = formatCurrency(bezos.finalWorth)
+
+	return (<div id="app">
+		<h1>What is Jeff Bezos worth? {netWorth}</h1>
+	</div>)
+}
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,18 @@
-import { h } from 'preact';
-import App from '../src/components/app';
+import { h } from 'preact'
+import { App } from '../src/components/app'
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
-import { shallow } from 'enzyme';
+import { shallow } from 'enzyme'
+import fetchMock from 'fetch-mock'
+import forbes400 from './__mocks__/forbes400.json'
 
-describe('Initial Test of the Header', () => {
-	test('Header renders 3 nav items', () => {
-		const context = shallow(<App />);
-		expect(context.find('h1').text()).toBe('What is Bezos worth?');
-	});
-});
+it('fetches the top of the Forbes 400', async () => {
+	fetchMock.mock('https://forbes400.herokuapp.com/api/forbes400?limit=10', forbes400)
+
+	const context = shallow(<App />)
+	await new Promise((resolve) => setTimeout(resolve, 0))
+	await new Promise((resolve) => setTimeout(resolve, 0))
+	await new Promise((resolve) => setTimeout(resolve, 0))
+
+	expect(context.find('h1').text()).toBe('What is Jeff Bezos worth? $109,600,795')
+	fetchMock.restore()
+})
