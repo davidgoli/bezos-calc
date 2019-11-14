f6d2dd784d58942b81f78e27cfea10b9565ed5d8
David Golightly <davigoli@gmail.com>
2019-11-13 21:18:54 -0800

    add build for gh pages

---
+++ build/precache-manifest.afae468b6ddf30d629b7f447b8cefe2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd1997835688b1ccb2f",
+    "url": "/bundle.56b50.css"
+  },
+  {
+    "revision": "bbd1997835688b1ccb2f",
+    "url": "/bundle.bbd19.js"
+  },
+  {
+    "revision": "4cef654beaea84fc0c048fb5bba7e029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d53525cd2f4e3d96678",
+    "url": "/polyfills.6d535.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "98be73b59694a63d24f2f2f0bbbb800a",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  }
+]);
---
+++ build/precache-manifest.e9ee652ef4d132e9a21727c91245e456.esm.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e884561ca71d93c7dacb43867cf7295f",
+    "url": "/bundle.53c81.esm.js"
+  },
+  {
+    "revision": "bbd1997835688b1ccb2f",
+    "url": "/bundle.56b50.css"
+  },
+  {
+    "revision": "4cef654beaea84fc0c048fb5bba7e029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8351a91fa35f18174cb90ebdf3766fd6",
+    "url": "/polyfills.66b25.esm.js"
+  }
+]);
